7bc930bcc156e328010725bd8d41ca64c8bc06f8
David <sm307560@gmail.com>
2024-04-11 00:59:14 -0500

    complete code

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Error({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="text-center my-4 bg-red-600 text-white font-bold uppercase p-3 text-sm">
+      {children}
+    </p>
+  );
+}
+
+export default Error;
